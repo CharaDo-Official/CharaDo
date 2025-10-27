fdc39dbe73adef0264203b1a7d3683c8a52e6d13
tipsy <tipsy@email.com>
2025-10-27 14:24:20 +0900

    #36 add 型連携確認

--- CharaDo/src/testComponent.tsx
+++ CharaDo/src/testComponent.tsx
@@ -1,12 +1,18 @@
 import { invoke } from "@tauri-apps/api/core";
 import { debug, warn } from "@tauri-apps/plugin-log";
 import { useEffect } from "react";
-
+import {Task} from "@bindings/task";
 
 function TaskTest() {
 	useEffect(() => {
 		invoke("get_all_tasks")
-			.then((tasks) => debug(`tasks: ${JSON.stringify(tasks)}`))
+			.then((tasks) => {
+				const taskList = tasks as Task[];
+				debug(`Fetched ${taskList.length} tasks:`);
+				taskList.forEach((task) => {
+					debug(`- [${task.id}] ${task.title} (created_date: ${task.created_date})`);
+				});
+			})
 			.catch((error) => warn(error));
 	}, []); // ← 空依存配列により初回マウント時のみ実行
 
