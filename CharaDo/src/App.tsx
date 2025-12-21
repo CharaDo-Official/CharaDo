3feaa7209824edc81e035dae8b74736813e34257
tipsy <tipsy@email.com>
2025-12-21 21:12:58 +0900

    #39 add UIをそれっぽく

--- CharaDo/src/App.tsx
+++ CharaDo/src/App.tsx
@@ -1,15 +1,9 @@
-import { info } from "@tauri-apps/plugin-log";
 import { useTab } from "@hooks/useTab";
 import { TabKey } from "@config/tabs";
 import TabBar from "@components/layout/TabBar";
 import TaskView from "@views/TaskView";
 import DebugView from "@views/DebugView";
 
-
-
-async function test() {
-	info("test button clicked");
-}
 
 function App() {
 	const { activeTab, selectTab } = useTab("task");
@@ -29,11 +23,6 @@ function App() {
 			<main>
 				{renderView(activeTab)}
 			</main>
-			<h1>Hello World!</h1>
-			<p>かわいいキャラクターがあなたのタスクを管理してくれる、可愛い×ToDoアプリ！</p>
-			<button onClick={test}>
-				Click me
-			</button>
 		</div>
 	);
 }
