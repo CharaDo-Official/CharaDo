591527adf1b77c7c7338c15d9fa317b3746b66d1
FastDefence <aaa100@outlook.jp>
2025-12-24 23:06:48 +0900

    #48 fix command/store.rsを修正

--- CharaDo/src-tauri/src/command/store.rs
+++ CharaDo/src-tauri/src/command/store.rs
@@ -6,3 +6,9 @@ pub async fn get_store_info() -> Result<StoreAppInfo, String> {
     tauri::async_runtime::spawn_blocking(|| fetch_store_info()).await
         .map_err(|e| format!("spawn_blocking join error: {:?}", e))?
 }
+
+#[tauri::command]
+pub async fn get_store_addons() -> Result<StoreAddOns, String> {
+    tauri::async_runtime::spawn_blocking(|| fetch_store_addons()).await
+        .map_err(|e| format!("spawn_blocking join error: {:?}", e))?
+}
